5ed38702813c7bcdf17533e9696c01df0d727d34
Nina Sophia <ninasophia.vetus.cics@ust.edu.ph>
2024-08-22 14:29:33 +0800

    product details update

--- capstone-final/src/App.jsx
+++ capstone-final/src/App.jsx
@@ -9,16 +9,20 @@ import CartPage from "./pages/CartPage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import Footer from "./global/Footer";
+import Header from "./global/header";
+import ProductDetail from "./product-page-component/ProductDetail";
 
 function App() {
   return (
     <Router>
+      <Header/>
       <Navbar />
       <Footer />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutUsPage />} />
         <Route path="/services" element={<ServicePage />} />
+        <Route path="/products" element={<ProductPage />} />
         <Route path="/products" element={<ProductPage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/signup" element={<SignUpPage />} />
